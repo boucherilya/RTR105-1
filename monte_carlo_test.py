e1ec2d1cf1304d1cb7592d1de3dd85a809ae019a
user <boucherilya@gmail.com>
2018-12-04 13:56:51 +0200

    RTR105-1

--- monte_carlo_test.py
+++ monte_carlo_test.py
@@ -9,32 +9,32 @@ from math import cos
 N = 100
 
 #pojavljauca 5 cisel randomnie ot 0 do 1
-x = random.uniform(0,5,N)
+x = random.uniform(0, 5, N)
 
 import sys
 sys.path.append('/usr/local/anaconda3/lib/python3.6/site-packages')
 from numpy import cos, linspace
-x = linspace(0,4,11)
+x = linspace(0, 4, 11)
 y = cos(x) * cos(x)
 from matplotlib import pyplot as plt
 plt.grid()
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.title('Funkcija $(cos(x)*cos(x))$')
-plt.plot(x,y,)
+plt.plot(x, y)
 plt.legend("$cos(x)$")
 plt.show()
 
-y = random.uniform(0,5,N)
-N1=0
+y = random.uniform(0, 5, N)
+N1 = 0
 from matplotlib import pyplot as plt
 plt.grid()
 for i in range(N):
-    print(x[i],y[i])
+    print(x[i], y[i])
     if x[i] > 2 and y[i] < 1:
         if y[i] < 1 and x[i] > y[i]:
-            plt.plot(x[i],y[i],'go')
+            plt.plot(x[i], y[i], 'go')
             N1 = N1 + 1
         else:
-            plt.plot(x[i],y[i],'ro')
+            plt.plot(x[i], y[i], 'ro')
 plt.show()
