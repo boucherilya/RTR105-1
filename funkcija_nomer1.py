b4f8a01c9751085c0d2f9bf14730b38e633fde03
user <boucherilya@gmail.com>
2018-12-18 14:00:26 +0200

    RTR105-1

--- funkcija_nomer1.py
+++ funkcija_nomer1.py
@@ -3,8 +3,19 @@ from math import cos
 
 #(1.) * (2.) = (2.) - float * int = float
 #(1.) * (2.) = (2.) - float * float = float#float(input()) -> float
-x = 1. * input("Lietotāj, lūdzu, ievadi argumentu (x): ")
+x = float( input("Lietotāj, lūdzu, ievadi argumentu (x): "))
 #print type(x)
+print ("x = %.f cos(x)*cos(x) = %.2f"%(x,cos(x)))
+
+print(" 500")
+print("------")
+print("\\              k+1  2k  2k-1")
+print(" \\          (-1)   *x  *2")
+print("    cos(2) => |-------------------")
+print(" /           (2*k)!")
+print("/")
+print("------")
+print(" k=1")
 
 y = (cos(x))**2
 print "(cos(x))**2(%.2f) = %.2f"%(x,y)
@@ -24,3 +35,22 @@ print "a2 = %.2f S2 = %.2f"%(a2,S2)
 a3  = (-1)**4*x**6*2**5/(1*2*3*4*5*6)
 S3 = a0 + a1 + a2 + a3
 print "a3 = %.2f S3 = %.2f"%(a3,S3)
+
+x = float(input("Lietotāj, lūdzu, ievadi argumentu (x): "))
+y = cos(x)*cos(x)
+
+print("cos(%.2f) = %.2f"%(x,y))
+a0 = (-1)**1*x**0*2**(-1)/(1)
+S = a0
+print("a0 = %.2f S0 = %.2f"%(a0,S0))
+
+S = S + a1
+print("a1 = %.2f S1 = %.2f"%(a1,S))
+
+a2 = (-1)**3*x**4*2**3/(1*2*3*4)
+S = S + a2
+print("a2 = %.2f S2 = %.2f"%(a2,S))
+
+a3 = (-1)**4*x**6*2**5/(1*2*3*4*5*6)
+S3 = S + a3
+print("a3 = %.2f S3 = %.2f"%(a3,S))
